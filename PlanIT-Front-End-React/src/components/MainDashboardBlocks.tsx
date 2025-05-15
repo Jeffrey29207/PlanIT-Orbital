7d22a028af046eb01345382bfacb229ccde6ff5b
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-05-15 10:43:22 +0700

    Add pieGraph, add classes to mainDashboardBlocks

--- PlanIT-Front-End-React/src/components/MainDashboardBlocks.tsx
+++ PlanIT-Front-End-React/src/components/MainDashboardBlocks.tsx
@@ -1,14 +1,14 @@
 function mainDashboardBlocks() {
   return (
     <>
-      <div className="mainDashboardBlocks overallDashboard"></div>
-      <div className="mainDashboardBlocks overallGraph"></div>
-      <div className="mainDashboardBlocks spendingDashboard"></div>
-      <div className="mainDashboardBlocks spendingGraph"></div>
-      <div className="mainDashboardBlocks transactionGraph"></div>
-      <div className="mainDashboardBlocks transactionHistory"></div>
-      <div className="mainDashboardBlocks savingDashboard"></div>
-      <div className="mainDashboardBlocks savingGraph"></div>
+      <div className="mainDashboardBlocks overallDashboard numericDashboard"></div>
+      <div className="mainDashboardBlocks overallGraph pieChart"></div>
+      <div className="mainDashboardBlocks spendingDashboard numericDashboard"></div>
+      <div className="mainDashboardBlocks spendingGraph pieChart"></div>
+      <div className="mainDashboardBlocks transactionGraph lineChart"></div>
+      <div className="mainDashboardBlocks transactionHistory history"></div>
+      <div className="mainDashboardBlocks savingDashboard numericDashboard"></div>
+      <div className="mainDashboardBlocks savingGraph pieChart"></div>
     </>
   );
 }

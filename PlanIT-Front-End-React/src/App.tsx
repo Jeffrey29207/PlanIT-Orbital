37469e62fdd98d39bc5f85c78663ff1350b53c09
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-05-15 08:20:05 +0700

    Add a section for spending graph

--- PlanIT-Front-End-React/src/App.tsx
+++ PlanIT-Front-End-React/src/App.tsx
@@ -24,10 +24,11 @@ function App() {
         <div className="mainDashboardBlocks overallDashboard"></div>
         <div className="mainDashboardBlocks overallGraph"></div>
         <div className="mainDashboardBlocks spendingDashboard"></div>
+        <div className="mainDashboardBlocks spendingGraph"></div>
         <div className="mainDashboardBlocks transactionGraph"></div>
+        <div className="mainDashboardBlocks transactionHistory"></div>
         <div className="mainDashboardBlocks savingDashboard"></div>
         <div className="mainDashboardBlocks savingGraph"></div>
-        <div className="mainDashboardBlocks transactionHistory"></div>
       </div>
     </>
   );

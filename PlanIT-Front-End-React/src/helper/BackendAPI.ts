4073b92923302f8e879e8dee20e12bacb99a5eb7
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-06-11 16:00:58 +0700

    Changed backend url to vercel

--- PlanIT-Front-End-React/src/helper/BackendAPI.ts
+++ PlanIT-Front-End-React/src/helper/BackendAPI.ts
@@ -1,7 +1,7 @@
 // These are functions to access fetch the backend API
 
-//const backendAPI = "https://plan-it-orbital-2025.vercel.app";
-const backendAPI = "http://localhost:8080";
+const backendAPI = "https://plan-it-orbital-2025.vercel.app";
+//const backendAPI = "http://localhost:8080";
 
 export const testUsers = async () => {
     return await fetch(`${backendAPI}/testUsers`).then(response => response.json());

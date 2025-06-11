6911ddc2470637200e9aab7efd5bcf77a89233f4
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-06-11 14:10:08 +0700

    Try using direct copy of supabase client info

--- PlanIT-Front-End-React/src/helper/config.ts
+++ PlanIT-Front-End-React/src/helper/config.ts
@@ -2,16 +2,16 @@
 
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+/*const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL or Anon Key is not defined in the environment variables.");
-}
+}*/
 
 const supabase =  createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    "https://tawzugumouawtstryldu.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd3p1Z3Vtb3Vhd3RzdHJ5bGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NjgyMDcsImV4cCI6MjA2MzA0NDIwN30.MuarXdMBYZECfpKDrOQPJ5hdhU1hLRYrUtChVvRoq3s"
 )
 
 export default supabase;

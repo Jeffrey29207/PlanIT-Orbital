47e05b427eff3ed08554c831f868b5f1a875a417
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-06-23 11:01:29 +0700

    Wrapped regressor_model in a js function

--- PlanIT_backend/MLModel/MLAndRecommFunc.js
+++ PlanIT_backend/MLModel/MLAndRecommFunc.js
@@ -1,6 +1,6 @@
 import ort from "onnxruntime-web"
 
-export default async function classify(data) {
+export async function classify(data) {
     const numberOfFeatures = data.length;
     const tensor = new ort.Tensor('float32', Float32Array.from(data), [1, numberOfFeatures]);
 
@@ -22,8 +22,35 @@ export default async function classify(data) {
     return prediction;
 }
 
-const testInput = [1533.63179194, 7554.03309488, 1435.69327901, 7170.55039617, 1303.13457716,
+export async function regression(data) {
+    const numberOfFeatures = data.length;
+    const tensor = new ort.Tensor('float32', Float32Array.from(data), [1, numberOfFeatures]);
+
+    // Load the pretrained model
+    const session = await ort.InferenceSession.create("Regressor_Model.onnx");
+
+    // Map the model input to the input tensor
+    const mapping = {float_input: tensor};
+
+    console.log(mapping);
+
+    const results = await session.run(mapping)
+
+    //get the output
+    const outputVar = Object.keys(results)[0];
+    const predictionBigInt = results[outputVar].data;
+    const prediction = Array.from(predictionBigInt, Number);
+    console.log(prediction)
+    return prediction;
+}
+
+const testInputClassify = [1533.63179194, 7554.03309488, 1435.69327901, 7170.55039617, 1303.13457716,
  7529.14483264, 1917.45378146, 9090.08344807, 1888.29391892, 5531.27976711,
  1615.6414697,  7375.01830777];
 
- classify(testInput);
+const testInputRegression = [1533.63179194, 7554.03309488, 1435.69327901, 7170.55039617, 1303.13457716,
+ 7529.14483264, 1917.45378146, 9090.08344807, 1888.29391892, 5531.27976711,
+ 1615.6414697,  7375.01830777, 2];
+
+ classify(testInputClassify);
+ regression(testInputRegression);

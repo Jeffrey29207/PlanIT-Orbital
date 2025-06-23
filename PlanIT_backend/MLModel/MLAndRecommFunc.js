2eb37c29127b303242ca2be706b7542dd423e35c
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-06-23 20:06:22 +0700

    Added gemini-2.5-flash for recommendation generation

--- PlanIT_backend/MLModel/MLAndRecommFunc.js
+++ PlanIT_backend/MLModel/MLAndRecommFunc.js
@@ -1,4 +1,6 @@
-import ort from "onnxruntime-web"
+import 'dotenv/config'
+import ort from "onnxruntime-web";
+import {GoogleGenAI} from "@google/genai";
 
 export async function classify(data) {
     const numberOfFeatures = data.length;
@@ -44,6 +46,45 @@ export async function regression(data) {
     return prediction;
 }
 
+export async function recommendation(spendingCategory, forecastedSpending, forecastedBalance) {
+    const agent = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+
+    const spendingLevel = spendingCategory === 0 
+                    ? "aggressive" 
+                    : spendingCategory === 1
+                    ? "mild"
+                    : "defensive";
+    const prompt = `
+    A user come to you with a financial profile specified as below:
+    1. The user's last 5 week spending is categorized as ${spendingLevel}.
+    2. Based on the last 5 week spending habit, the user's spending on the next week is forecasted to be around ${forecastedSpending} Singapore Dollar.
+    3. Based on the last 5 week spending habit, the user's balance on the next week is forecasted to be around ${forecastedBalance} Singapore Dollar.
+
+    Based on the user profile stated above, advise the user with executable steps to either maintain or imporve the user's financial health.
+    The advise must begin with a summary of the user financial profile and how the user's current profile will effect the user's financial condition in the future.
+    The advise must not exceed 200 words.
+    The advise must be stated in a step by step manner.
+    End the advise with an encouraging statement
+    `
+
+    const recommendationText = await agent.models.generateContent(
+        {
+            model: "gemini-2.5-flash",
+            contents: prompt,
+            config: {
+                thinkingConfig: {
+                    thinkingBudget: 0,
+                },
+                systemInstruction: "You are a professional financial advisor. You must directly give advise as what the prompt is asking you. You are not allowed to ask any question."
+            }
+        }
+    )
+
+    console.log(recommendationText.text);
+
+    return recommendationText.text;
+}
+
 const testInputClassify = [1533.63179194, 7554.03309488, 1435.69327901, 7170.55039617, 1303.13457716,
  7529.14483264, 1917.45378146, 9090.08344807, 1888.29391892, 5531.27976711,
  1615.6414697,  7375.01830777];
@@ -52,5 +93,6 @@ const testInputRegression = [1533.63179194, 7554.03309488, 1435.69327901, 7170.5
  7529.14483264, 1917.45378146, 9090.08344807, 1888.29391892, 5531.27976711,
  1615.6414697,  7375.01830777, 2];
 
- classify(testInputClassify);
- regression(testInputRegression);
+ //classify(testInputClassify);
+ //regression(testInputRegression);
+await recommendation(0,8932.45378146,9090.08344807);

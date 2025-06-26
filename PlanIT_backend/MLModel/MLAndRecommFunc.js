092ecb0a473d2b369afe20b0279f8a01977238c6
MrMarshall12 <assabilachmadfirdaus@gmail.com>
2025-06-27 05:20:24 +0700

    Removed console logging

--- PlanIT_backend/MLModel/MLAndRecommFunc.js
+++ PlanIT_backend/MLModel/MLAndRecommFunc.js
@@ -13,16 +13,13 @@ export async function classify(data) {
     // Map the model input to the input tensor
     const mapping = {float_input: tensor};
 
-    console.log(mapping);
-
     const results = await session.run(mapping)
 
     //get the output
     const outputVar = Object.keys(results)[0];
     const predictionBigInt = results[outputVar].data;
     const prediction = Array.from(predictionBigInt, Number);
-    console.log(prediction[0])
-    return prediction;
+    return prediction[0];
 }
 
 export async function regression(data) {
@@ -36,8 +33,6 @@ export async function regression(data) {
 
     // Map the model input to the input tensor
     const mapping = {float_input: tensor};
-
-    console.log(mapping);
 
     const results = await session.run(mapping)
 
@@ -95,5 +90,6 @@ const testInputClassify = [1533.63179194, 7554.03309488, 1435.69327901, 7170.550
 const testInputRegression = [1533.63179194, 7554.03309488, 1435.69327901, 7170.55039617, 1303.13457716,
  7529.14483264, 1917.45378146, 9090.08344807, 1888.29391892, 5531.27976711,
  1615.6414697,  7375.01830777, 2];
+
 
  
